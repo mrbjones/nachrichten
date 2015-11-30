6d6925ba2a4dd0b99f55cd41053b2f265cb5ee9a
mrbjones <mrbjones@users.noreply.github.com>
2015-11-30 11:12:45 -0700

    Update app.js

--- app.js
+++ app.js
@@ -6,17 +6,27 @@ var options = {
   host: 'http://www.welt.de',
   path: '/?service=Rss'
 };
-callback = function(response) {
-  var str = '';
-  response.on('data', function (chunk) {
-  str += chunk;
-  });
-  response.on('end', function () {
-  res.write(str);;
-  });
+function fetchNachrichten(callback){
+return http.get({
+        host: 'personatestuser.org',
+        path: '/email'
+    }, function(response) {
+        // Continuously update stream with data
+        var body = '';
+        response.on('data', function(d) {
+            body += d;
+        });
+        response.on('end', function() {
+            callback({
+              body
+            });
+        });
+    });
+
+
 }
 
 var server = http.createServer(function(req, res) {
 res.writeHead(200, {'Content-Type': 'text/plain'});
-http.request(options, callback).end();
+fetchNachrichten(callback);
 }).listen(process.env.VCAP_APP_PORT);
