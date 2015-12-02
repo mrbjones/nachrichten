95d74b659bc7c61da0656c1a6e2f5d936070f818
mrbjones <mrbjones@users.noreply.github.com>
2015-12-02 16:16:20 -0700

    Update app.js

--- app.js
+++ app.js
@@ -23,9 +23,13 @@ return http.get({
       var parser = new xml2js.Parser();      
       parser.parseString(body.substring(0, body.length), function (err, result) {
       var json = JSON.stringify(result, ["rss", "$", "channel", "item", "title", "link", "category", "pubDate", "description", "guid"]);
-     for(var item in json){
-     callback(item[title]);
-     }
+    for(var i = 0; i < json.length; i++) {
+    var obj = json[i];
+
+    callback(obj.title);
+}
+   
+     
      
     //  callback(json);
     });
