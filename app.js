c184ce343ccb2756fd3895a74925bfebab169d4b
mrbjones <mrbjones@users.noreply.github.com>
2015-12-02 21:14:24 -0700

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ return http.get({
       var json = JSON.stringify(result, ["rss", "$", "channel", "item", "title", "link", "category", "pubDate", "description", "guid"]);
         // var json1 = JSON.parse(json);
        
-      callback(json.rss);
+      callback(json[rss]);
 
      
         });
