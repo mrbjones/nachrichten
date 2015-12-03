6266ac6e19231bb3dd74695e9cfbe0c8b1d16b02
mrbjones <mrbjones@users.noreply.github.com>
2015-12-02 19:29:55 -0700

    Update app.js

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ return http.get({
       parser.parseString(body.substring(0, body.length), function (err, result) {
       var json = JSON.stringify(result, ["rss", "$", "channel", "item", "title", "link", "category", "pubDate", "description", "guid"]);
          var obj1 = JSON.parse(json);
-         callback(obj1.channel.item.guid)
+         callback(obj1.rss.'$'.channel.item.guid)
  
          
          //callback(obj1.channel.item.guid); 
