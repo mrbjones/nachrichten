9f86922d2c7e266b20e4ba5f3bc8b5399f4d77bd
mrbjones <mrbjones@users.noreply.github.com>
2015-12-02 18:54:18 -0700

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ return http.get({
       var parser = new xml2js.Parser();      
       parser.parseString(body.substring(0, body.length), function (err, result) {
       var json = JSON.stringify(result, ["rss", "$", "channel", "item", "title", "link", "category", "pubDate", "description", "guid"]);
-         body.forEach(function(obj) { callback(item.guid); });
+         body.forEach(function(obj) { callback(obj.item.guid); });
      
     });
        //  callback(extractedData);
