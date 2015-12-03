b6640ae89b734da15bec3b1c191f473fa02ff61c
mrbjones <mrbjones@users.noreply.github.com>
2015-12-03 12:41:21 -0700

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ orchestrate_api_endpoint = node.credentials.ORCHESTRATE_API_HOST
 };
 var db = require("orchestrate")(orchestrate_api_key,orchestrate_api_endpoint);
 function putter(title,link,category,pubDate,description,guid,cb) {
-var jsonString = "{\"title\":\"" +jsesc(title, {'quotes': 'double'});+ "\", \"link\":\""+jsesc(link, {'quotes': 'double'});+"\", \"category\":\""+category+"\", \"pubDate\":\""+pubDate+"\", \"description\":\""+jsesc(description, {'quotes': 'double'});+"\"}";
+var jsonString = "{\"title\":\"" +jsesc(title.replace(/"/g, "'");, {'quotes': 'double'});+ "\", \"link\":\""+jsesc(link, {'quotes': 'double'});+"\", \"category\":\""+category+"\", \"pubDate\":\""+pubDate+"\", \"description\":\""+jsesc(description.replace(/"/g, "'");, {'quotes': 'double'});+"\"}";
 cb(jsonString)
 var jsonObj = JSON.parse(jsonString);
 //db.put('nachrichten', guid, jsonObj, false);
