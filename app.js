8d7e2054c9ddc51d171165e613611c414babea11
mrbjones <mrbjones@users.noreply.github.com>
2015-12-03 18:34:14 -0700

    Update app.js

--- app.js
+++ app.js
@@ -16,36 +16,28 @@ orchestrate_api_endpoint = node.credentials.ORCHESTRATE_API_HOST
 }
 };
 var db = require("orchestrate")(orchestrate_api_key,orchestrate_api_endpoint);
-function putter(title,link,category,pubDate,description,guid,cb) {
+function putter(title,link,category,pubDate,description,guid) {
         var jsonTitle=title.toString().replace(/\"/g,'\\"');
         var jsonDesc=description.toString().replace(/\"/g,'\\"');
 var jsonString = "{\"title\":\"" +jsonTitle+ "\", \"link\":\""+link+"\", \"category\":\""+category+"\", \"pubDate\":\""+pubDate+"\", \"description\":\""+jsonDesc+"\"}";
-
-cb(jsonString)
 var jsonObj = JSON.parse(jsonString);
 db.put('nachrichten', guid, jsonObj, false);
-//cb("success :!");
 };
 
-function fetchNachrichten(callback){
+function fetchNachrichten(){
 return http.get({
         host: 'www.welt.de',
         path: '/?service=Rss'
     }, function(response) {
-        // Continuously update stream with data
         var body = '';
         response.on('data', function(d) {
             body += d;
         });
         response.on('end', function() {
-         /*   callback(body); */
          var extractedData = "";
          var parser = new xml2js.Parser();
          parser.parseString(body, function(err,result){
-    //Extract the value from the data element
-    //     extractedData = result['rss']['channel']['item']['guid'];
-    //     extractedData = result.xml.channel.title;
-      var parser = new xml2js.Parser();      
+       var parser = new xml2js.Parser();      
       parser.parseString(body.substring(0, body.length), function (err, result) {
       var json = JSON.stringify(result, ["rss", "$", "channel", "item", "title", "link", "category", "pubDate", "description", "guid"]);
       var json1 = JSON.parse(json);
@@ -64,7 +56,7 @@ return http.get({
                                      if (bbb=="description"){ description=json1[rss][xxx][yyy][zzz][aaa][bbb];   }
                                        if (bbb=="guid"){  guid=json1[rss][xxx][yyy][zzz][aaa][bbb] }
  }
-  putter(title,link,category,pubDate,description,guid,callback);
+  putter(title,link,category,pubDate,description,guid);
                                
                        }
     
@@ -77,8 +69,15 @@ return http.get({
         });
         });
         };
-
+/*
 var server = http.createServer(function(req, res) {
 res.writeHead(200, {'Content-Type': 'text/plain'});
 fetchNachrichten( function(resp) { res.write(resp);res.end(); }); 
 }).listen(process.env.VCAP_APP_PORT);
+*/
+var minutes = 15, the_interval = minutes * 60 * 1000;
+setInterval(function() {
+  fetchNachrichten();
+}, the_interval);
+
+
