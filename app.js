238a536a0558acc32084fe74c03386bb4ae9d9f3
mrbjones <mrbjones@users.noreply.github.com>
2015-12-03 17:22:06 -0700

    Update app.js

--- app.js
+++ app.js
@@ -29,7 +29,8 @@ cb(JSON.stringify(items))
 
 
 var server = http.createServer(function(req, res) {
-res.writeHead(200, {'Content-Type': 'text/plain'});
+res.writeHead(200, {'Content-Type': 'text/plain;charset=UTF-8'});
+
 getter( function(resp)
 {res.write("<br>" + resp);res.end();
 }); 
