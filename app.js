72ecac93550f807e6c1da5b2180143a0e6bde954
mrbjones <mrbjones@users.noreply.github.com>
2015-12-22 13:50:50 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 var http = require('http');
 var url = require('url');
 var express = require('express');
-//var xml2js = require('xml2js');
-//var jsesc = require('jsesc');
 var fs = require("fs");
 var path = require("path");
 var mime = require("mime");
@@ -10,48 +8,26 @@ var mime = require("mime");
 if (process.env.VCAP_SERVICES)
 {
 var services = JSON.parse(process.env.VCAP_SERVICES);
-
 var orchestrateConfig = services["orchestrate"];
 if (orchestrateConfig) {
 var node = orchestrateConfig[0];
 orchestrate_api_key = node.credentials.ORCHESTRATE_API_KEY
 orchestrate_api_endpoint = node.credentials.ORCHESTRATE_API_HOST
-}
-};
+}};
 var db = require("orchestrate")(orchestrate_api_key,orchestrate_api_endpoint);
 
 function getter(cb) {
 db.search('nachrichten', '*', {  sort: 'value.pubDate:desc',  limit: 15} )
 .then(function (result) {
-//var items = result.body.results;
-// cb(JSON.stringify(items, ['path', 'key', 'value', 'title', 'description', 'category', 'pubDate', 'link']));
-//cb(JSON.stringify(items))
 cb(JSON.stringify(result))
 })};
 
 function searcher(a,b,cb) {
 db.search('nachrichten', a, {  sort: 'value.pubDate:desc',  limit: 15, offset: b} )
 .then(function (result) {
-//var items = result.body.results;
 // cb(JSON.stringify(items, ['path', 'key', 'value', 'title', 'description', 'category', 'pubDate', 'link']));
-//cb(JSON.stringify(result.count))
-//cb(JSON.stringify(items))
 cb(JSON.stringify(result))
- 
- 
 })};
-
-
- /*
-var server = http.createServer(function(req, res) {
-res.writeHead(200, {'Content-Type': 'text/plain;charset=UTF-8'});
-
-getter( function(resp)
-{res.write("<br>" + resp);res.end();
-}); 
-}).listen(process.env.VCAP_APP_PORT);
-*/
-
 
 
 function send404(response) {
@@ -95,8 +71,11 @@ searcher(queryData.search+'*', offs, function(resp)
 {response.write(resp);response.end();
 }); }
 
+
+
+
 //this one sends the page!
-if (queryData.o != "g" && queryData.o != "s") {
+if (queryData.o == "" || ! queryData.o ) {
 var filePath = false;
 if (request.url == '/') {
 filePath = "public/index.html";
