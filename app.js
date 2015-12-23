dd8f40fb5f7072023393b31a6ba65ab9e0da9c83
mrbjones <mrbjones@users.noreply.github.com>
2015-12-23 15:43:07 -0700

    Update app.js

--- app.js
+++ app.js
@@ -146,8 +146,8 @@ if (queryData.o == "logg") {
 response.writeHead(200, {'Content-Type': 'text/plain;charset=UTF-8'});
 loggIn(queryData.user, queryData.passw, function(resp)
 {
-     response.cookie('hesher', resp, { maxAge: 900000, httpOnly: true })
-     response.cookie('user', queryData.user, { maxAge: 900000, httpOnly: true })
+  //   response.cookie('hesher', resp, { maxAge: 900000, httpOnly: true })
+  //   response.cookie('user', queryData.user, { maxAge: 900000, httpOnly: true })
      response.write(resp);response.end();
 }); }
 
