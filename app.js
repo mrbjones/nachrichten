58b0d387baafd35baa9d1b61e6fd9d1fd3dc3c0c
mrbjones <mrbjones@users.noreply.github.com>
2015-12-28 22:29:11 -0700

    Update app.js

--- app.js
+++ app.js
@@ -185,7 +185,6 @@ var filePath = false;
 if (request.url == '/') {
 filePath = "public/login.html";
 } else {
-     console.log(request.url) 
 filePath = "public" + request.url;
 }
 var cookies = new Cookies( request, response )
@@ -194,7 +193,7 @@ var ha1='123'
 em1=cookies.get("email")
 ha1=cookies.get("hash")
 // console.log('e:'+em1+':h:'+ha1)
-if (request.url == "/index.html"||request.url == "index.html"||request.url=='/'||request.url=''||request.url='public/index.html'||!request.url) {
+if (request.url == "/index.html"||request.url == "index.html"||request.url=="/"||request.url==""||request.url=="public/index.html"||!request.url) {
 checker(em1, ha1, function(resp) {
 if (resp == "true"){filePath = "public/index.html"}
 else {filePath = "public/login.html"}
