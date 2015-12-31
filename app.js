2cf79a83d2834d29dc717c34c550f0d91dd6ca5f
mrbjones <mrbjones@users.noreply.github.com>
2015-12-30 19:14:15 -0700

    Update app.js

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ db.newPatchBuilder('users', user)
      if (result.body.statusr != 'active'){cb(2)}
      if (result.body.username==undefined) {cb(3)}
      if (result==undefined) {cb(3)}
-}).fail(cb(3))
+}).fail(function (err) {console.log(err)})
 }}
 
 function checker(user,hash,cb){
