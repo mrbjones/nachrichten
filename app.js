63539ca510e2640a091ca13f57e97b54fcc2e279
mrbjones <mrbjones@users.noreply.github.com>
2016-01-08 15:48:32 -0700

    Update app.js

--- app.js
+++ app.js
@@ -48,8 +48,8 @@ function getLike(user) {
 //searcher="@path.source.collection:'users' AND @path.source.key:'"+user+"' AND @path.destination.collection:'nachrichten' AND @path.destination.key:'http://www.welt.de/?config=articleidfromurl&artid=150784491' AND @path.relation:('marked')"
 //searcher='@path.kind:relationship AND @path.source.collection:users AND @path.source.key:mrbrettjones@gmail.com'
 //searcher='@path.kind:relationship AND @path.source.key:mrbrettjones@gmail.com AND @path.destination.key:http://www.zeit.de/gesellschaft/2016-01/aegypten-angriff-touristen-verletzt-terror' 
-searcher="@path.kind:relationship AND @path.destination.key:http://www.zeit.de/gesellschaft/2016-01/aegypten-angriff-touristen-verletzt-terror" 
-searcher='@path.kind:relationship AND @path.source.key:mrbrettjones@gmail.com AND @path.destination.collection:nachrichten'
+//searcher="@path.kind:relationship AND @path.destination.key:http://www.zeit.de/gesellschaft/2016-01/aegypten-angriff-touristen-verletzt-terror" 
+searcher="@path.kind:relationship AND (@path.source.key:mrbrettjones@gmail.com AND @path.destination.collection:'nachrichten')"
 
   db.newSearchBuilder()
   .query(searcher)
@@ -62,7 +62,7 @@ searcher='@path.kind:relationship AND @path.source.key:mrbrettjones@gmail.com AN
             marks = relres.body;
             console.log(JSON.stringify(marks));
       })
-      .fail(function (err) {console.log(JSON.stringify(err.Code));})
+      .fail(function (res1) {console.log(JSON.stringify(res1.code));})
         
 }
 
