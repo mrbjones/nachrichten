c56a54bfabec7a6474935080f4ff3643af24544c
mrbjones <mrbjones@users.noreply.github.com>
2016-01-08 15:10:09 -0700

    Update app.js

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ function getLike(user) {
 */
 //searcher="@path.source.collection:'users' AND @path.source.key:'"+user+"' AND @path.destination.collection:'nachrichten' AND @path.destination.key:'http://www.welt.de/?config=articleidfromurl&artid=150784491' AND @path.relation:('marked')"
 //searcher='@path.kind:relationship AND @path.source.collection:users AND @path.source.key:mrbrettjones@gmail.com'
-searcher='@path.kind:relationship AND @path.source.key:mrbrettjones@gmail.com AND @path.destination.key:"http://www.zeit.de/gesellschaft/2016-01/aegypten-angriff-touristen-verletzt-terror"' 
+searcher='@path.kind:relationship AND @path.source.key:mrbrettjones@gmail.com AND @path.destination.key:http://www.zeit.de/gesellschaft/2016-01/aegypten-angriff-touristen-verletzt-terror' 
   db.newSearchBuilder()
   .query(searcher)
 //db.newGraphReader()
