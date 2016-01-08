971897aabaebb6d3b66a07960c76093a1cc9ee49
mrbjones <mrbjones@users.noreply.github.com>
2016-01-08 14:37:37 -0700

    Update app.js

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ function getLike(user) {
         })
 */
 //searcher="@path.source.collection:'users' AND @path.source.key:'"+user+"' AND @path.destination.collection:'nachrichten' AND @path.destination.key:'http://www.welt.de/?config=articleidfromurl&artid=150784491' AND @path.relation:('marked')"
-searcher="@path.source.kind:'relationship' AND @path.source.collection:'users' AND @path.source.key:'"+user+"'"
+searcher='@path.kind:relationship AND @path.source.collection:users AND @path.source.key:"+user+""
   db.newSearchBuilder()
 
     .query(searcher)
