8469ee8778fb04dfd25710d18087d411b7c21e6d
mrbjones <mrbjones@users.noreply.github.com>
2016-01-08 16:17:58 -0700

    Update app.js

--- app.js
+++ app.js
@@ -47,9 +47,9 @@ function getLike(user) {
 */
 //searcher="@path.source.collection:'users' AND @path.source.key:'"+user+"' AND @path.destination.collection:'nachrichten' AND @path.destination.key:'http://www.welt.de/?config=articleidfromurl&artid=150784491' AND @path.relation:('marked')"
 //searcher='@path.kind:relationship AND @path.source.collection:users AND @path.source.key:mrbrettjones@gmail.com'
-//searcher='@path.kind:relationship AND @path.source.key:mrbrettjones@gmail.com AND @path.destination.key:http://www.zeit.de/gesellschaft/2016-01/aegypten-angriff-touristen-verletzt-terror' 
+searcher='@path.kind:relationship AND @path.source.key:mrbrettjones@gmail.com AND @path.destination.key:`http://www.zeit.de/gesellschaft/2016-01/aegypten-angriff-touristen-verletzt-terror`' 
 //searcher="@path.kind:relationship AND @path.destination.key:http://www.zeit.de/gesellschaft/2016-01/aegypten-angriff-touristen-verletzt-terror" 
-searcher='@path.kind:relationship AND (@path.destination.kind:item)'
+//searcher='@path.kind:relationship AND (@path.destination.kind:item)'
 
   db.newSearchBuilder()
   .query(searcher)
