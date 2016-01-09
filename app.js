c7637f1f97d61b65f013d1f986545a066cc57a37
mrbjones <mrbjones@users.noreply.github.com>
2016-01-09 08:40:46 -0700

    Update app.js

--- app.js
+++ app.js
@@ -38,15 +38,9 @@ cb(JSON.stringify(result))
 function getLike(user) {
 db.search('nachrichten', '*', {  sort: 'value.pubDate:desc',  limit: 15} )
 .then(function (result) {
-var obj=result.body
-for (var link in obj) {
-    if (obj.hasOwnProperty(link)) {
-        switch (link) {
-            console.log(obj[link])
-        }
-    }
-}
-      
+
+console.log(result.body.link)
+
 searcher='@path.kind:relationship AND @path.source.key:'+user
 searcher=searcher+' AND (@path.destination.key:`http://www.zeit.de/gesellschaft/2016-01/aegypten-angriff-touristen-verletzt-terror`)' 
 db.newSearchBuilder()
